725055545421b29a8b336f761255a4022b4ff692
Grigory Bakunov <thebobuk@ya.ru>
2021-04-12 22:00:58 +0300

    fix all the linters error

--- tests/test_ubase.py
+++ tests/test_ubase.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
-import pytest
 import asyncio
+import sys
+
+import pytest
 
 sys.path.append(".")
+
 
 @pytest.fixture(scope="module")
 def event_loop():
@@ -12,45 +14,50 @@ def event_loop():
     yield loop
     loop.close()
 
+
 @pytest.mark.asyncio
 async def test_base_keys():
     import ubase
+
     db = await ubase.init_db(":memory:")
     for i in range(10):
         await db.put(f"base:{i}", i)
 
     res = []
-    async for k,v in db.keys(ubase.OP.GT, "base:5", 'base:'):
+    async for k, v in db.keys(ubase.OP.GT, "base:5", "base:"):
         res.append(v)
     assert len(res) == 4
     assert res[1] == 7
 
     res = []
-    async for k,v in db.keys(ubase.OP.LTE, "base:5", 'base:', limit=4):
+    async for k, v in db.keys(ubase.OP.LTE, "base:5", "base:", limit=4):
         res.append(v)
     assert len(res) == 4
     assert res[-1] == 2
 
     res = []
-    async for k,v in db.keys("<", "base:3", 'base:', limit=1):
+    async for k, v in db.keys("<", "base:3", "base:", limit=1):
         res.append(v)
     assert len(res) == 1
     assert res[0] == 2
 
     await db.close()
-    
+
 
 @pytest.mark.asyncio
 async def test_base_simpe():
     import ubase
+
     db = await ubase.init_db(":memory:")
     await db.put("test", "t")
     assert "t" == await db.get("test")
     await db.close()
-    
+
+
 @pytest.mark.asyncio
 async def test_base_upsert():
     import ubase
+
     db = await ubase.init_db(":memory:")
     await db.put("test", "t")
     assert "t" == await db.get("test")
@@ -58,32 +65,38 @@ async def test_base_upsert():
     assert 1 == await db.get("test")
     await db.close()
 
+
 @pytest.mark.asyncio
 async def test_base_defaults():
     import ubase
-    db = await ubase.init_db(":memory:", defaults = {"tuser": "pass"})
+
+    db = await ubase.init_db(":memory:", defaults={"tuser": "pass"})
     assert "pass" == await db.get("tuser")
     assert "test" == await db.get("key", "test")
     await db.close()
 
+
 @pytest.mark.asyncio
 async def test_base_proxy():
     import ubase
+
     db = await ubase.init_db(":memory:")
     await db.area.put("test", "pass")
     assert "pass" == await db.area.get("test")
     assert "pass" == await db.get("area:test")
     await db.close()
 
+
 @pytest.mark.asyncio
 async def test_base_keys_proxy():
     import ubase
+
     db = await ubase.init_db(":memory:")
     for i in range(10):
         await db.area.put(i, i)
 
     res = []
-    async for k,v in db.area.keys("<=", 5, limit = 3):
+    async for k, v in db.area.keys("<=", 5, limit=3):
         res.append(v)
     print(res)
     assert len(res) == 3
--- ubase.py
+++ ubase.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-from typing import Optional, List, Dict, Union, Tuple, AsyncGenerator
-
 import asyncio
 from enum import Enum
+from typing import AsyncGenerator, Dict, Optional, Tuple, Union
 
 import aiosqlite
 
 try:
     import ujson as json
-except:
-    import json # type: ignore
+except ModuleNotFoundError:
+    import json  # type: ignore
 
 KeyType = Union[int, str]
 ValueType = Union[int, str, dict, list]
@@ -37,15 +36,6 @@ class OP(Enum):
 
 _OP_LIST = {OP.GT: ">", OP.GTE: ">=", OP.LT: "<", OP.LTE: "<="}
 
-import inspect
-
-x, y, z = 1, 2, 3
-
-
-def retrieve_name(var):
-    callers_local_vars = inspect.currentframe().f_back.f_locals.items()
-    return [var_name for var_name, var_val in callers_local_vars if var_val is var]
-
 
 class uBaseProxy:
     def __init__(self, base: "uBase", mask: str):
@@ -65,6 +55,7 @@ class uBaseProxy:
     ) -> AsyncGenerator[Tuple[KeyType, ValueType], None]:
         return self.base.keys(op, f"{self.mask}:{key}", self.mask, limit)
 
+
 class uBase:
     def __init__(self, db: str):
         self.dbname = db
@@ -79,24 +70,27 @@ class uBase:
     async def get(
         self, key: KeyType, default: Optional[ValueType] = None
     ) -> Optional[ValueType]:
-        if not self.db: raise NotInitialized
+        if not self.db:
+            raise NotInitialized
         async with self.db.execute(
-            f"SELECT data FROM kvbase WHERE id=?", (key,)
+            "SELECT data FROM kvbase WHERE id=?", (key,)
         ) as cursor:
             data = await cursor.fetchone()
             if data:
                 try:
                     return json.loads(data[0])
-                except:
+                except TypeError:
                     return data[0]
             else:
                 return default
 
     async def put(self, key: KeyType, data: ValueType):
-        if not self.db: raise NotInitialized
+        if not self.db:
+            raise NotInitialized
         dt = json.dumps(data)
         await self.db.execute(
-            "INSERT INTO kvbase(id, data) VALUES (?,?) ON CONFLICT(id) DO UPDATE SET data=?",
+            "INSERT INTO kvbase(id, data) VALUES (?,?) "
+            + "ON CONFLICT(id) DO UPDATE SET data=?",
             (key, dt, dt),
         )
 
@@ -107,7 +101,8 @@ class uBase:
         mask: Optional[KeyType] = "",
         limit: int = -1,
     ) -> AsyncGenerator[Tuple[KeyType, ValueType], None]:
-        if not self.db: raise NotInitialized
+        if not self.db:
+            raise NotInitialized
         operator: str = ""
         if type(op) == str:
             operator = str(op)
@@ -116,7 +111,11 @@ class uBase:
                 raise NoOperations
             operator = _OP_LIST[OP(op)]
         order = "desc" if "<" in operator else "asc"
-        req = f"SELECT id, data FROM kvbase WHERE (id {operator} '{key}') and (id LIKE '{mask}%') order by id {order} limit {limit}"
+        req = (
+            "SELECT id, data FROM kvbase WHERE "
+            + f"(id {operator} '{key}') and (id LIKE '{mask}%')"
+            + f"order by id {order} limit {limit}"
+        )
         print(req)
         async with self.db.execute(req) as cursor:
             async for key, item in cursor:
@@ -124,18 +123,19 @@ class uBase:
         return
 
     async def close(self):
-        if not self.db: raise NotInitialized
+        if not self.db:
+            raise NotInitialized
         await self.db.close()
 
 
-async def init_db(dbname: str, defaults: Dict[KeyType, ValueType] = {}) -> uBase:
-    DB = uBase(dbname)
-    DB.db = await aiosqlite.connect(dbname, isolation_level=None)
+async def init_db(name: str, defaults: Dict[KeyType, ValueType] = {}) -> uBase:
+    DB = uBase(name)
+    DB.db = await aiosqlite.connect(name, isolation_level=None)
     try:
         await DB.db.execute(
-            "CREATE TABLE kvbase (id varchar(32) PRIMARY KEY UNIQUE, data json)"
+            "CREATE TABLE kvbase " + "(id varchar(32) PRIMARY KEY UNIQUE, data json)"
         )
-    except:
+    except aiosqlite.OperationalError:
         raise CantCreateDatabase
     for k, v in defaults.items():
         if not await DB.get(k):
@@ -144,13 +144,13 @@ async def init_db(dbname: str, defaults: Dict[KeyType, ValueType] = {}) -> uBase
 
 
 async def main():
-    db = await init_module("test.db")
+    db = await init_db("test.db")
     await db.put("asdf:0001", "a")
     await db.put("asdf:0002", "b")
     await db.put("asdf:0003", "c")
     await db.put("asdf:0004", "d")
     await db.put("asdf:0005", "e")
-    async for key in db.keys(Operators.GT, "asdf:0002", "asdf:"):
+    async for key in db.keys(OP.GT, "asdf:0002", "asdf:"):
         print(key)
     print(await db.get("asdf:0003"))
 
